957ae76535b449b75c5206fb908fa300bb3815e6
ArtemBay <kkunfin@gmail.com>
2022-12-24 20:51:31 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #This is config file for YandexMusic and Discord Applications
 
 #Yandex token. You can get it from "get_yandex_token.py"
-token = "Your TOKEN HERE"
+token = ""
 
 # This is Discord Aplications ID
 #You can get it from https://discord.com/developers/applications/
 
-discord_application = 'paste id here'
+discord_application = '1041197129726689301'
--- main.py
+++ main.py
@@ -1,53 +1,98 @@
 from pypresence import Presence
 from yandex_music import Client
+#import yandex_music
 from config import *
 import time
+import os
+import threading
+import sys
 
-#Yandex token. You can get it from "get_yandex_token.py"
+import win32gui
+import win32con
+from SysTray import SysTrayIcon
+
+def bye(sysTrayIcon):
+    RPC.clear()
+    sys.exit(1)
+
+def hide(sysTrayIcon):
+    RPC.clear()
+    global RPCSTATUS
+    RPCSTATUS = False
+
+def show(sysTrayIcon):
+    global RPCSTATUS
+    RPCSTATUS = True
+    mp = threading.Thread(target=music)
+    mp.daemon = True
+    mp.start()
+
 TOKEN = token
 
-# This is Aplications ID
 client_id = discord_application
 
-# Inicalize the RPC  
 RPC = Presence(client_id=client_id)
 RPC.connect()
 
-# Inicalize Yandex Music API
 client = Client(TOKEN).init()
 
-# Work until the program doesnt stoped
-while True:  
-    try:
-        queues = client.queues_list()
-        last_queue = client.queue(queues[0].id)
-        last_track_id = last_queue.get_current_track()
-        last_track = last_track_id.fetch_track()
-        artists = ', '.join(last_track.artists_name())
-        title = last_track.title
-        track_link = f"https://music.yandex.ru/album/{last_track['albums'][0]['id']}/track/{last_track['id']}/"
-        image_link="https://" + last_track.cover_uri.replace("%%", "1000x1000")
-        btns = [
-                { 
-            "label": "Слушать Трек",
-            "url": track_link
-                }
-        ]
+def plus():
+    if client.me.plus.has_plus: return "Плюс активирован!"
+    else: return "Плюс не активирован"
 
-        RPC.update(
-            details="Слушает: " + title,
-            state="Исполнитель: " + artists,
-            large_image=image_link,
-            small_image="Link for small image if you want",
-            large_text="Your Text Here",
-            small_text="Your Text Here",
-            buttons=btns  
-        )
-    except:
-        RPC.update(
-        details='Поддерживаются только треки из плейлистов 😥',
-        state='Попробуй включить трек из плейлистов 🙃',
-        large_image='https://c.tenor.com/ZuIbNWpIN5MAAAAC/rias-gremory-high-school-dxd.gif'
-        )
-        
-    time.sleep(1) # update the rpc status (in seconds), default: 1 
+def plusimg():
+    if client.me.plus.has_plus: return "https://yastat.net/s3/plus/landing/2022/icons/rebrand/apple-touch-icon-180x180.png"
+    else: return "https://assets.stickpng.com/images/5897a8c3cba9841eabab6156.png"
+
+def music():
+    while RPCSTATUS:
+        try:
+            queues = client.queues_list()
+            last_queue = client.queue(queues[0].id)
+            last_track_id = last_queue.get_current_track()
+            last_track = last_track_id.fetch_track()
+            artists = ', '.join(last_track.artists_name())
+            title = last_track.title
+            track_link = f"https://music.yandex.ru/album/{last_track['albums'][0]['id']}/track/{last_track['id']}/"
+            image_link="https://" + last_track.cover_uri.replace("%%", "1000x1000")
+            btns = [
+                    { 
+                "label": "Слушать Трек",
+                "url": track_link
+                    }
+            ]
+
+            RPC.update(
+                details="Слушает: " + title,
+                state="Исполнитель: " + artists,
+                large_image=image_link,
+                large_text="Че ты на меня наводишь свой грязный кусрсор?!??!?",
+                buttons=btns,
+                small_image=plusimg(),
+                small_text=plus()
+            )
+        except:
+            RPC.update(
+            details='Поддерживаются только треки из плейлистов 😥',
+            state='Ну что мне поделать с яндексом 🙃',
+            large_image='https://media.tenor.com/S7SLFLHTy-wAAAAC/uh-sorry.gif',
+            large_text="Яндекс музыка не дает получить трек не из плейлиста xD",
+            small_image=plusimg(),
+            small_text=plus()
+            )
+    
+        time.sleep(5)
+
+def main():
+    icon = os.path.join(os.path.dirname(os.path.abspath(__file__)), "icon_main.ico")
+    hover_text = "Yandex Music RPC"
+    menu_options = (("Show status", None, show), ("Hide status", None, hide))
+    mp = threading.Thread(target=music)
+    mp.daemon = True
+    mp.start()
+    SysTrayIcon(icon, hover_text, menu_options, on_quit=bye, default_menu_index=1)
+
+
+if __name__ == "__main__":
+    RPCSTATUS = True
+    main()
